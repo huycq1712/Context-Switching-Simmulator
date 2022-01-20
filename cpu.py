b57a69b70e44db0d63790f26cb066594b1a21b52
huycq1712 <huysk82000@gmail.com>
2022-01-20 16:38:15 +0700

    update docstring

--- cpu.py
+++ cpu.py
@@ -5,9 +5,11 @@ from sched import *
 
 
 class CPU:
-    def __init__(self, QTtime):
+    """Mô phỏng một CPU với bộ lập dịch, bộ decoder và các hàng đợi
+    """
+    def __init__(self, QTtime, maxPID = 100):
         self.clock = 0
-        self.pidmanager = PIDmanager()
+        self.pidmanager = PIDmanager(maxPID)
         self.decoder = Decoder(self)
         self.register = { "_R1":0,
                           "_R2":0,
@@ -23,6 +25,8 @@ class CPU:
         self.CurTask = None
     
     def rescheduler(self):
+        """Tái lập lịch
+        """
         self.FinishTask = self.RunQueue.deQueue().task_struct
         self.CurTask = self.RunQueue.header.next.task_struct
         if self.RunQueue.num != 0:
@@ -32,24 +36,42 @@ class CPU:
             print("------------------------------------------------------------------------")
         
     def swap_in(self, process):
+        """Cơ chế Swap in trong context switch
+
+        Args:
+            process (task_struct)): Tiến trình được swap in
+        """
         self.register["_R1"] = process.process_context.register["_R1"]
         self.register["_R2"] = process.process_context.register["_R2"]
         self.register["_R3"] = process.process_context.register["_R3"]
         self.register["_R4"] = process.process_context.register["_R4"]
         
     def swap_out(self, process):
+        """Cơ chế Swap out trong context switch
+
+        Args:
+            process (task_struct): Tiến trình bị swap out
+        """
         process.process_context.register["_R1"] = self.register["_R1"]
         process.process_context.register["_R2"] = self.register["_R2"]
         process.process_context.register["_R3"] = self.register["_R3"]
         process.process_context.register["_R4"] = self.register["_R4"]
         
     def remove_from_tasklist(self, process):
+        """Xóa một tiến trình khỏi TaskList khi nó hoàn thành
+
+        Args:
+            process (task_struct): task_struct của tiến trình cần xóa
+        """
         for indx ,task in enumerate(self.task_list):
             if task.pid == process.pid:
                 self.task_list.pop(indx)
     
     def IO_handle(self):
+        """Xử lý tiến trình khi có yêu cầu I/O
+        """
         print("------------------------------------------------------------------------")
+        print("                Đưa tiến trình Process {} vào WaitQueue                 ".format(self.CurTask.pid))
         self.CurTask.state = TASK_STATE["WAITING"]
         self.RunQueue.deQueue()
         self.WaitQueue.enQueue(Node(self.CurTask))
@@ -63,9 +85,11 @@ class CPU:
             self.CurTask = None
         print("RunQueue:", self.RunQueue.get_id_process())
         print("WaitQueue:", self.WaitQueue.get_id_process())
-        print("-------------------------------------------------------------------------")
+        print("------------------------------------------------------------------------")
         
     def release(self):
+        """Giải phóng thanh ghi khi tiến trình chạy xong
+        """
         self.register["_R1"] = 0
         self.register["_R2"] = 0
         self.register["_R3"] = 0
@@ -73,8 +97,10 @@ class CPU:
     
     
 class PIDmanager:
-    def __init__(self):
-        self.max = 100
+    """Quản lý các giá trị PID
+    """
+    def __init__(self, maxPid=100):
+        self.max = maxPid
         self.max_cur = 0
         self.used_pid = [0]*self.max
     
