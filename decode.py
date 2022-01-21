e2c158d4e944b9104c43de30d011da44d24a4936
huycq1712 <huysk82000@gmail.com>
2022-01-21 17:11:58 +0700

    update docsting

--- decode.py
+++ decode.py
@@ -5,6 +5,14 @@ class Decoder:
         self.cpu = cpu
     
     def convert_to_number(self, variable):
+        """Kiểm tra xem tham số hàm là chữ só, biến hay thanh ghi, nếu là biến hoặc thành ghi thì sẽ trả về giá trị của nó
+
+        Args:
+            variable (string): Tên tham số  hàm (ví dụ a, b, _R1)
+
+        Returns:
+            int: giá trị tương ứng với tham số
+        """
         if variable.isnumeric():
             return int(variable)
         if variable.startswith("_R"):
@@ -13,6 +21,14 @@ class Decoder:
             return self.cpu.CurTask.stack[variable]
     
     def set_instr(self, instr):
+        """Thực thi hàm set gán một giá trị cho biến
+
+        Args:
+            instr (string): Câu lệnh set [] []
+
+        Returns:
+            [None]: None
+        """
         args = instr.split(" ")
         if args[1].startswith("_R"):
             self.cpu.register[args[1]] = self.convert_to_number(args[2])
@@ -21,6 +37,14 @@ class Decoder:
         return None
     
     def add_instr(self, instr):
+        """Thực thi lệnh add
+
+        Args:
+            instr (string): Câu lệnh add [] [] []
+
+        Returns:
+            [None]: None
+        """
         args = instr.split(" ")
         if args[3].startswith("_R"):
             self.cpu.register[args[3]] = self.convert_to_number(args[1]) + self.convert_to_number(args[2])
@@ -29,6 +53,14 @@ class Decoder:
         return None
     
     def sub_instr(self, instr):
+        """Thực hiện lệnh sub
+
+        Args:
+            instr (string): Câu lệnh sub
+
+        Returns:
+            [None]: None
+        """
         args = instr.split(" ")
         if args[3].startswith("_R"):
             self.cpu.register[args[3]] = self.convert_to_number(args[1]) - self.convert_to_number(args[2])
@@ -37,6 +69,14 @@ class Decoder:
         return None
             
     def mul_instr(self, instr):
+        """Thực hiện lệnh mul
+
+        Args:
+            instr ([type]): Câu lệnh mul
+
+        Returns:
+            None: None
+        """
         args = instr.split(" ")
         if args[3].startswith("_R"):
             self.cpu.register[args[3]] = self.convert_to_number(args[1]) * self.convert_to_number(args[2])
@@ -45,14 +85,35 @@ class Decoder:
         return None
     
     def IOrq_instr(self, instr):
+        """Yêu cầu I/O
+
+        Args:
+            instr (string): Câu lệnh iorequest
+
+        Returns:
+            [int]: trả về 2 để đánh giá 
+        """
         args = instr.split(" ")
         self.cpu.IO_FLAG = 1
         return 2
             
     def end_instr(self, args):
+        """Kết thúc chương trình
+
+        Args:
+            args (string): Câu lệnh end
+
+        Returns:
+            int: Báo hiệu tiến trình kêt thúc
+        """
         return 1
     
     def excute(self):
+        """Thực thi lệnh
+
+        Returns:
+            int: Trả về giá trị thông báo cho hệ thống 1 là tiến trình kết thúc 2 yêu cầu IO
+        """
         instr = self.cpu.CurTask.instrucMem[self.cpu.CurTask.pc]
         args = instr.split(" ")
         instr_list = {
